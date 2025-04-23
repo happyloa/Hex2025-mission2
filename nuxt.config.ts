6a58ede127b71490cf9e1c93f91c660777d907f7
Aaron <piafyoyo06@gmail.com>
2025-04-23 23:47:51 +0800

    feat: 新增 Google Font 設定，將網站字型改為思源黑體

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,18 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2024-11-01',
+  compatibilityDate: "2024-11-01",
   devtools: { enabled: true },
-  modules: ['@nuxtjs/tailwindcss', '@nuxtjs/google-fonts']
-})
+  modules: ["@nuxtjs/tailwindcss", "@nuxtjs/google-fonts"],
+
+  // Google Fonts 的相關配置
+  googleFonts: {
+    families: {
+      "Noto+Sans+TC": [100, 200, 300, 400, 500, 600, 700, 800, 900],
+    },
+    download: true,
+    inject: true,
+    display: "swap",
+  },
+
+  css: ["~/assets/css/fonts.css"],
+});
