4799a230e3b2c54f2e77ea7a7915eb2c01266f9e
Aaron <piafyoyo06@gmail.com>
2025-04-24 02:36:27 +0800

    feat: 新增網站 meta 設定

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,6 +2,16 @@
 export default defineNuxtConfig({
   compatibilityDate: "2024-11-01",
   devtools: { enabled: true },
+
+  // 網站 meta 設定
+  app: {
+    head: {
+      title: "體驗營最終任務 - 個人品牌網站",
+      htmlAttrs: {
+        lang: "zh-Hant",
+      },
+    },
+  },
   modules: ["@nuxtjs/tailwindcss", "@nuxtjs/google-fonts"],
 
   // Google Fonts 的相關配置
