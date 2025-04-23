19a5c3d8a62861b2a440d7e7a828764fcf75d367
Aaron <piafyoyo06@gmail.com>
2025-04-24 01:03:03 +0800

    style: 擴充標準色

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -4,7 +4,12 @@ export default <Partial<Config>>{
   theme: {
     extend: {
       colors: {
-        bgc: "#FBFBFB",
+        bgc: {
+          DEFAULT: "#FBFBFB",
+          dark: "#F1F1F1",
+        },
+        content: "#4B4B4B",
+        blue: "#0027D5",
       },
       fontSize: {
         "fs-3": [
