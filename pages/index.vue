5baa6bccb28b3b67f0039ea44eac16865c194cb3
Aaron <piafyoyo06@gmail.com>
2025-04-24 07:00:28 +0800

    feat: 完成首頁 Hero 區塊

--- pages/index.vue
+++ pages/index.vue
@@ -11,4 +11,13 @@ useSeoMeta({
 });
 </script>
 
-<template><CommonHero /></template>
+<template>
+  <CommonHero title="Alyse Wang" subtitle="前端工程師 & 職涯諮詢師" />
+  <section class="relative bg-bgc px-3 pb-16 pt-[356px] md:pb-20 md:pt-[282px]">
+    <AtomIntro />
+    <div class="mx-auto max-w-[1296px]">
+      <h2 class="text-head-2-sm md:text-head-2 mb-8">專業服務與方案</h2>
+      <main></main>
+    </div>
+  </section>
+</template>
