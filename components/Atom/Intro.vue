24c48efb800f5ccf052ff4941d0659ae1c44f033
Aaron <piafyoyo06@gmail.com>
2025-04-24 06:58:44 +0800

    feat: 新增自我介紹元件

---
+++ components/Atom/Intro.vue
@@ -0,0 +1,28 @@
+<template>
+  <div
+    class="text-fs-3-light absolute -top-40 right-0 mx-3 flex flex-col gap-4 bg-white px-6 py-10 lg:max-w-[1077px] lg:px-10 lg:py-20 xl:right-20"
+  >
+    <p>嗨，我是 Alyse 一名深耕前端技術的工程師。</p>
+    <p>
+      擅長 React、Vue
+      等框架，同時熱愛為轉職與新手工程師提供職涯指導。邀請你與我一起，開啟更具潛能的程式與職涯之旅！
+    </p>
+    <ul class="flex gap-1">
+      <li class="transition-all hover:-translate-y-1">
+        <a href="#" target="_blank"><img src="/icon/youtube.webp" /></a>
+      </li>
+      <li class="transition-all hover:-translate-y-1">
+        <a href="#" target="_blank"><img src="/icon/podcast.webp" /></a>
+      </li>
+      <li class="transition-all hover:-translate-y-1">
+        <a href="#" target="_blank"><img src="/icon/facebook.webp" /></a>
+      </li>
+      <li class="transition-all hover:-translate-y-1">
+        <a href="#" target="_blank"><img src="/icon/linkedin.webp" /></a>
+      </li>
+      <li class="transition-all hover:-translate-y-1">
+        <a href="#" target="_blank"><img src="/icon/instagram.webp" /></a>
+      </li>
+    </ul>
+  </div>
+</template>
