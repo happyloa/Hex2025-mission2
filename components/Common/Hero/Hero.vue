e95714f50e4f9e8b2c9451d49fe1d42d85d8eccf
Aaron <piafyoyo06@gmail.com>
2025-04-24 06:23:50 +0800

    feat: 完成主要 Hero 元件

--- components/Common/Hero/Hero.vue
+++ components/Common/Hero/Hero.vue
@@ -1,6 +1,41 @@
+<script setup lang="ts">
+/**
+ * @typedef {Object} Props
+ * @property {string} title - 主要標題，會顯示於圖片右側的 H1 元素中
+ * @property {string} subtitle - 副標題，會顯示於 H1 下方的段落文字
+ */
+defineProps({
+  title: {
+    type: String,
+    default: "請輸入標題",
+  },
+  subtitle: {
+    type: String,
+    default: "請輸入副標題",
+  },
+});
+</script>
+
 <template>
-  <section class="flex">
-    <img src="/desktop/home/hero-image.webp" alt="人物形象照" />
-    <div class="pl-20"></div>
+  <section class="flex flex-col lg:flex-row">
+    <figure>
+      <picture>
+        <source
+          media="(max-width: 640px)"
+          srcset="/mobile/home/hero-image.webp"
+        />
+        <img
+          src="/desktop/home/hero-image.webp"
+          alt="人物形象照"
+          class="w-full"
+        />
+      </picture>
+    </figure>
+    <div
+      class="flex flex-1 flex-col justify-center gap-3 bg-[url('/mobile/home/hero-deco.webp')] bg-cover px-6 pb-[200px] pt-6 text-blue md:bg-[url('/desktop/home/hero-deco.webp')] lg:px-20 lg:py-0"
+    >
+      <h1 class="text-head-1-sm lg:text-head-1">{{ title }}</h1>
+      <p class="text-fs-3">{{ subtitle }}</p>
+    </div>
   </section>
 </template>
