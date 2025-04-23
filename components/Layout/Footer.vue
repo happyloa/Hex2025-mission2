059007d739c94882c85691dd6895a80ac8de49d2
Aaron <piafyoyo06@gmail.com>
2025-04-24 00:05:47 +0800

    feat: 新增導覽列與頁尾

---
+++ components/Layout/Footer.vue
@@ -0,0 +1,5 @@
+<template>
+  <footer class="text-fs-6 px-10 py-6 bg-white text-center">
+    <p>© 2025 Alyse Wang. All rights reserved.</p>
+  </footer>
+</template>
---
+++ components/Layout/Header.vue
@@ -0,0 +1,10 @@
+<template>
+  <header class="py-6 bg-bgc sticky top-0">
+    <nav>
+      <ul class="text-fs-3 flex flex-wrap gap-x-10 gap-y-2 justify-center">
+        <li><NuxtLink to="/">首頁</NuxtLink></li>
+        <li><NuxtLink to="/blog">部落格</NuxtLink></li>
+      </ul>
+    </nav>
+  </header>
+</template>
