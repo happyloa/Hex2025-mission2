81b0d21bd4b5dfaa5cdb40da09de66c60cb53c24
Aaron <piafyoyo06@gmail.com>
2025-04-24 08:53:26 +0800

    style: 設定 z-index

--- components/Layout/Header.vue
+++ components/Layout/Header.vue
@@ -1,5 +1,5 @@
 <template>
-  <header class="sticky top-0 bg-bgc py-6">
+  <header class="sticky top-0 z-50 bg-bgc py-6">
     <nav>
       <ul class="flex flex-wrap justify-center gap-x-10 gap-y-2 text-fs-3">
         <li><NuxtLink to="/">首頁</NuxtLink></li>
