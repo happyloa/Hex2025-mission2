0a93369493488c1d27e41bae0bc7a5392f208571
Aaron <piafyoyo06@gmail.com>
2025-04-24 18:58:04 +0800

    feat: 定義文章 collection

---
+++ content.config.ts
@@ -0,0 +1,17 @@
+import { defineCollection, z } from "@nuxt/content";
+
+export const collections = {
+  // 把 blog 目錄下所有 md 當成 posts 集合
+  blog: defineCollection({
+    type: "page",
+    source: "blog/**/*.md",
+    schema: z.object({
+      title: z.string(),
+      description: z.string(),
+      date: z.date(),
+      cover: z.string(),
+      path: z.string(),
+      tags: z.array(z.string()),
+    }),
+  }),
+};
