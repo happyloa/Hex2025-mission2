a401216fa8ce036ea59aa4f467def86c37987af4
Aaron <piafyoyo06@gmail.com>
2025-04-24 20:38:06 +0800

    style: 設定最大寬度

--- components/Common/SocialLinks.vue
+++ components/Common/SocialLinks.vue
@@ -1,5 +1,5 @@
 <template>
-  <section class="hidden bg-bgc px-3 py-20 md:block">
+  <section class="mx-auto hidden max-w-[1920px] bg-bgc px-3 py-20 md:block">
     <div class="mx-auto flex max-w-[1296px] items-center justify-between gap-6">
       <a href="mailto:alysewang@hexschool.com" class="text-fs-2 hover:underline"
         >alysewang@hexschool.com</a
