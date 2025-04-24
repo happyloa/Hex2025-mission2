a0450fcd76b1aa2ca6a234bdeca482fc7df6636c
Aaron <piafyoyo06@gmail.com>
2025-04-24 10:01:58 +0800

    feat: 新增職涯諮詢成功案例客戶圖片

---
+++ components/Common/CustomerCard.vue
@@ -0,0 +1,41 @@
+<script setup lang="ts">
+/**
+ * @typedef {Object} Props
+ * @property {string} image - 圖片內容
+ * @property {string} title - 成功案例標題
+ * @property {Array} list - 案例列表項目
+ */
+defineProps({
+  image: {
+    type: String,
+    default: "avatar1",
+  },
+  title: {
+    type: String,
+    default: "請輸入案例標題",
+  },
+  list: {
+    type: Array,
+    default: ["項目1", "項目2"],
+  },
+});
+</script>
+
+<template>
+  <article>
+    <img :src="`/avatar/${image}.webp`" alt="客戶圖片" class="mb-4" />
+    <h3 class="text-fs-4-bold mb-2">{{ title }}</h3>
+    <ul
+      class="mb-4 list-image-[url(avatar/check.webp)] pl-6 text-fs-6 text-content"
+    >
+      <li v-for="(item, index) in list" :key="index">{{ item }}</li>
+    </ul>
+    <hr class="mb-4 border-b border-content" />
+    <button
+      type="button"
+      class="rounded-full border border-black px-4 py-2 text-fs-6 text-content transition-all hover:bg-black hover:text-[#F5F5F5]"
+    >
+      前往聆聽 podcast
+    </button>
+  </article>
+</template>
