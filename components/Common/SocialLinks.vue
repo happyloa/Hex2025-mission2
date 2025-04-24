28777b7c2226e35575770c96605791cff49972e6
Aaron <piafyoyo06@gmail.com>
2025-04-24 20:35:25 +0800

    feat: 新增社群連結區塊元件

---
+++ components/Common/SocialLinks.vue
@@ -0,0 +1,36 @@
+<template>
+  <section class="hidden bg-bgc px-3 py-20 md:block">
+    <div class="mx-auto flex max-w-[1296px] items-center justify-between gap-6">
+      <a href="mailto:alysewang@hexschool.com" class="text-fs-2 hover:underline"
+        >alysewang@hexschool.com</a
+      >
+      <ul class="flex gap-1">
+        <li class="transition-all hover:-translate-y-1">
+          <a href="#" target="_blank"
+            ><img src="/icon/youtube.webp" alt="youtube icon"
+          /></a>
+        </li>
+        <li class="transition-all hover:-translate-y-1">
+          <a href="#" target="_blank"
+            ><img src="/icon/podcast.webp" alt="podcast icon"
+          /></a>
+        </li>
+        <li class="transition-all hover:-translate-y-1">
+          <a href="#" target="_blank"
+            ><img src="/icon/facebook.webp" alt="facebook icon"
+          /></a>
+        </li>
+        <li class="transition-all hover:-translate-y-1">
+          <a href="#" target="_blank"
+            ><img src="/icon/linkedin.webp" alt="linkedin icon"
+          /></a>
+        </li>
+        <li class="transition-all hover:-translate-y-1">
+          <a href="#" target="_blank"
+            ><img src="/icon/instagram.webp" alt="instagram icon"
+          /></a>
+        </li>
+      </ul>
+    </div>
+  </section>
+</template>
