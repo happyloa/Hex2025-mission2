b1c570edd2f1462c8d061e62fcca0678f90c113a
Aaron <piafyoyo06@gmail.com>
2025-04-25 07:54:31 +0800

    feat: 製作單篇文章頁面，用 ContentRenderer 元件將文章內容顯示出來

--- pages/blog/[post].vue
+++ pages/blog/[post].vue
@@ -1,4 +1,29 @@
+<script setup>
+const route = useRoute();
+
+// 用 useAsyncData + queryCollection 拿單篇文章
+//   - .path() 直接過濾 collection 裡 path 等於 route.path 的文件
+//   - .first() 把結果回傳成一個物件而不是陣列
+const { data: post } = await useAsyncData(`${route.path}`, () =>
+  queryCollection("blog").path(route.path).first(),
+);
+</script>
+
 <template>
   <CommonHeroBlog hidePreview />
-  單篇部落格文章 {{ $route.params.post }}
+  <section
+    v-if="post"
+    class="mx-auto max-w-[1920px] border border-secondary bg-bgc px-3 py-16 md:py-20"
+  >
+    <article class="prose mx-auto max-w-[636px]">
+      <!-- 正文，ContentRenderer 會自動把 Markdown 轉成 HTML + Vue 元件 -->
+      <ContentRenderer :value="post" />
+    </article>
+  </section>
+
+  <!-- loading / not found 處理 -->
+  <p v-else class="py-10 text-center text-fs-2 text-content">
+    文章載入中或不存在🥲
+  </p>
+  <CommonSocialLinks />
 </template>
