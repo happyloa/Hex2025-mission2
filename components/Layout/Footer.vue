134de7a298d8c4d78222b841c4bb27caa49a0a77
Aaron <piafyoyo06@gmail.com>
2025-05-18 18:57:23 +0800

    feat: 新增 GitHub repo 連結

--- components/Layout/Footer.vue
+++ components/Layout/Footer.vue
@@ -1,7 +1,14 @@
 <template>
   <footer
-    class="mx-auto max-w-[1920px] bg-white px-10 py-6 text-center text-fs-6"
+    class="mx-auto flex max-w-[1920px] flex-col items-center gap-2 bg-white px-10 py-6 text-center text-fs-6"
   >
     <p>© 2025 Alyse Wang. All rights reserved.</p>
+    <a href="https://github.com/happyloa/Hex2025-mission2" target="_blank">
+      <img
+        src="/public/icon/github-icon.webp"
+        alt="GitHub repo 連結"
+        class="size-12"
+      />
+    </a>
   </footer>
 </template>
