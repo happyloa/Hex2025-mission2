ca02dc6f3f4e047d620e3ecd41370e0f4b993524
Aaron <piafyoyo06@gmail.com>
2025-05-22 13:01:02 +0800

    style: 在特定條件下讓選單文字呈現標準色的藍色

--- components/Layout/Header.vue
+++ components/Layout/Header.vue
@@ -2,8 +2,22 @@
   <header class="sticky top-0 z-50 mx-auto max-w-[1920px] bg-bgc py-6">
     <nav>
       <ul class="flex flex-wrap justify-center gap-x-10 gap-y-2 text-fs-3-bold">
-        <li><NuxtLink to="/">首頁</NuxtLink></li>
-        <li><NuxtLink to="/blog">部落格</NuxtLink></li>
+        <li>
+          <NuxtLink
+            to="/"
+            class="duration-300 hover:text-blue"
+            :class="$route.path === '/' ? 'text-blue' : ''"
+            >首頁</NuxtLink
+          >
+        </li>
+        <li>
+          <NuxtLink
+            to="/blog"
+            class="duration-300 hover:text-blue"
+            :class="$route.path.includes('blog') ? 'text-blue' : ''"
+            >部落格</NuxtLink
+          >
+        </li>
       </ul>
     </nav>
   </header>
